349adbee7c730dd15cce9f53941e2dba0837fd3a
bramirez1325 <109931545+bramirez1325@users.noreply.github.com>
2023-01-31 22:23:05 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Duane's Farmers Only</title>
+</head>
+<nav class="duanes-title">
+  <h1>Duane's Only Farmers</h1>
+  <ul class="nav_link--list>
+    <li class="nav_link">
+      <a href="#">About</a>
+    </li>
+    <li class="nav_link">
+      <a href="#">Crops<span>😏</span></a>
+    </li>
+    <li class="nav_link">
+      <a href="#">Chester White</a>
+    </li>
+  </ul>
+</nav>
+<div>
+  <img class="black-duane center" src="/Assests/Only farmers.jpg"></img>
+  <p>Let me farm your bush ma'am</p>
+</div>
+`;
